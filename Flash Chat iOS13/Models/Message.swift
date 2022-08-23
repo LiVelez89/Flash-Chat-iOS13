1eb6560f319d506427172a35b20efaeff4e81227
Jorge Restrepo <livelez89@gmail.com>
2022-08-22 22:52:26 -0500

    Base Project

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Lina on 20/08/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //1@2.com
+    let body: String
+}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Lina on 21/08/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
